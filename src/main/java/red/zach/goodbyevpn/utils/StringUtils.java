eccd2573ce739086eee7e54f30d1428e0e66cd39
Zach/Shadow <presentingtulip@gmail.com>
2017-01-30 18:12:35 -0500

    Update StringUtils.java

--- src/main/java/red/zach/goodbyevpn/utils/StringUtils.java
+++ src/main/java/red/zach/goodbyevpn/utils/StringUtils.java
@@ -2,9 +2,6 @@ package red.zach.goodbyevpn.utils;
 
 import org.bukkit.ChatColor;
 
-/**
- * Created by 18zzwerling on 1/30/17.
- */
 public class StringUtils {
 
     public String convertString(String s){
