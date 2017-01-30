9246fad4698c931d841277fd7ffdb9d57424627d
Zach/Shadow <presentingtulip@gmail.com>
2017-01-30 18:12:24 -0500

    Update Main.java

--- src/main/java/red/zach/goodbyevpn/Main.java
+++ src/main/java/red/zach/goodbyevpn/Main.java
@@ -3,9 +3,6 @@ package red.zach.goodbyevpn;
 import org.bukkit.plugin.java.JavaPlugin;
 import red.zach.goodbyevpn.listeners.JoinListener;
 
-/**
- * Created by 18zzwerling on 1/30/17.
- */
 public class Main extends JavaPlugin {
     public static Main instance;
 
